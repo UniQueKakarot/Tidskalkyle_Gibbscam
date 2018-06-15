52db33c5906a41e43219858b83c677f7e3bc0f66
Iver Ullrik Hepsø <iverullrik92@gmail.com>
2018-06-15 14:54:10 +0200

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ This script is going to be timecalculating my machiningtime,
     and outputting the results in a excelfile """
 
-with open('Time.txt') as f:
+with open('Time4.txt') as f:
     filebody = [line for line in f]
 
 filebody_clean = [item[:-1] for item in filebody]
@@ -11,7 +11,9 @@ filebody_clean = [float(item) for item in filebody_clean]
 
 time_in_sec = 0.0
 for time in filebody_clean:
-    time_in_sec += time 
+    time_in_sec += time
+
+time_in_sec = round(time_in_sec, 0)
 
 print(time_in_sec)
 
@@ -19,4 +21,21 @@ seconds = 0
 minutes = 0
 hours = 0
 
+while time_in_sec != 0.0:
 
+    if time_in_sec > 3600.0:
+        hours += 1
+        time_in_sec -= 3600.0
+
+    elif time_in_sec > 60.0:
+        minutes += 1
+        time_in_sec -= 60.0
+
+    else:
+        seconds = time_in_sec
+        time_in_sec -= time_in_sec
+
+print('Hours: ', hours)
+print('Minutes: ', minutes)
+print('Seconds: ', seconds)
+print(time_in_sec)
